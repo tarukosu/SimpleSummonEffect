fd0863fb8ce31463bbf232aeb26d07cb703835b1
tarukosu <tarukosu0@gmail.com>
2017-03-05 20:55:19 +0900

    commit all

---
+++ Assets/SimpleSummonEffect/Scripts/SummonEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartEffect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartEffect()
+    {
+        foreach (Transform child in transform)
+        {
+            var particle = child.gameObject.GetComponent<ParticleSystem>();
+            if (particle)
+            {
+                particle.Play();
+            }
+        }
+    }
+}
